4b798182769bc907e04e328cbd77ab289b0606e2
awersh2s <andreas.wershofen@smail.inf.h-brs.de>
2018-06-29 16:20:50 +0200

    -Unnoetige imports entfernt

--- hbrs.login/src/main/java/org/h_brs/coolemicroservices/services/LoginService.java
+++ hbrs.login/src/main/java/org/h_brs/coolemicroservices/services/LoginService.java
@@ -3,9 +3,7 @@ package org.h_brs.coolemicroservices.services;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.h_brs.coolemicroservices.facade.dto.AccountTO;
 import org.h_brs.coolemicroservices.services.pdo.Account;
-import org.hibernate.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
