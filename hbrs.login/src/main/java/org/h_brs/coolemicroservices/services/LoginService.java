8e5afcd237646f2e15eb9d7f5f08abaffa651514
awersh2s <andreas.wershofen@smail.inf.h-brs.de>
2018-06-29 16:00:29 +0200

    -LoginService implementiert -Account geaendert

--- hbrs.login/src/main/java/org/h_brs/coolemicroservices/services/LoginService.java
+++ hbrs.login/src/main/java/org/h_brs/coolemicroservices/services/LoginService.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.h_brs.coolemicroservices.facade.dto.AccountTO;
+import org.h_brs.coolemicroservices.services.pdo.Account;
+import org.hibernate.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,10 +15,20 @@ public class LoginService {
 	EntityManager em;
 	
 	@Transactional
-	public AccountTO getAccount() {
+	public Account getAccount(String email) {
 		
-		//TODO: Rueckgabe hinzufuegen
-		return null;
+		javax.persistence.Query query = em.createNamedQuery("Account.findAccountByName");
+		query.setParameter(0, email);
+		
+		Account acc = null;
+		
+		try {
+			acc = (Account) query.getSingleResult();		
+		}catch(Exception e) {
+			System.out.println("Kein Account mit der EMail vorhanden: "+email);
+		}
+		
+		return acc;
 		
 	}
 }
--- hbrs.login/src/main/java/org/h_brs/coolemicroservices/services/pdo/Account.java
+++ hbrs.login/src/main/java/org/h_brs/coolemicroservices/services/pdo/Account.java
@@ -5,10 +5,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+	@NamedQuery(name = "Account.findAccountByName", query = "SELECT a FROM Account a WHERE EMAIL = ?")
+	})
 @Table(name = "ACCOUNT")
 public class Account {
 	
