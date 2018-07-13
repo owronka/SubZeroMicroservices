459b0434f0c2902a343c55fcf23013aa139263ae
awersh2s <andreas.wershofen@smail.inf.h-brs.de>
2018-07-13 14:45:08 +0200

    -registration target ignored

--- hbrs.registration/target/test-classes/testDatensaetze.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO app.person(1,"Herr","Oliver","Wronka");
-INSERT INTO app.account(1,"wronka@axxessio.com","owronka","test1234",1)
-INSERT INTO app.adresse(1,"DE","53177","Kurfürstenalle","5",1)
-
-INSERT INTO app.person(2,"Frau","Olga","Wronkalein");
-INSERT INTO app.account(2,"wronkalein@wasanderes.com","owronkalein","1234test",2)
-INSERT INTO app.adresse(2,"EN","23EF 32","Englandstreet","683",2)
