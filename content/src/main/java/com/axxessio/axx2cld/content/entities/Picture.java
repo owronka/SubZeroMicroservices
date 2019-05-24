e9c46320c19e93e03908c9d101c6c3e28af2d5ef
Dengeleng <thomas_herent@gmx.net>
2019-05-24 14:20:35 +0200

    dependencies ergaenzt

--- content/src/main/java/com/axxessio/axx2cld/content/entities/Picture.java
+++ content/src/main/java/com/axxessio/axx2cld/content/entities/Picture.java
@@ -10,8 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
+//import lombok.Getter;
+//import lombok.Setter;
 
 @Entity
 @Table(name = "tbl_picture")
--- content/src/main/java/com/axxessio/axx2cld/content/entities/SubArea.java
+++ content/src/main/java/com/axxessio/axx2cld/content/entities/SubArea.java
@@ -7,8 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
+//import lombok.Getter;
+//import lombok.Setter;
 
 
 @Entity
