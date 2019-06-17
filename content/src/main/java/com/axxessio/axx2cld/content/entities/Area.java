73729207e8c9b51eeffa14ad423dc47e72beea8e
Dengeleng <thomas_herent@gmx.net>
2019-06-17 17:44:37 +0200

    Content-Fix

--- content/src/main/java/com/axxessio/axx2cld/content/entities/Area.java
+++ content/src/main/java/com/axxessio/axx2cld/content/entities/Area.java
@@ -1,35 +1,23 @@
 package com.axxessio.axx2cld.content.entities;
 
-import java.io.Serializable;
+import org.springframework.data.annotation.Id;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "tbl_area")
 public class Area{
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer id;
-	@Column(nullable=false, name="name", unique = true)
+	
+	@Id private String id;
 	private String name;
-
+	
 	public Area() {
 	}
 
-	public Integer getId() {
-		return this.id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
+    public String getId() {
+    	return this.id;
+    }
+    
+    public void setId(String id) {
+    	this.id=id;
+    }
+    
 	public String getName() {
 		return this.name;
 	}
