0db75ef0c34eda248f66b506142aefc029f91eed
valentin <valentin.liu@smail.inf.h-brs.de>
2019-11-15 16:54:35 +0100

    new

--- registration/src/main/java/com/hbrs/project/registration/RestController.java
+++ registration/src/main/java/com/hbrs/project/registration/RestController.java
@@ -1,3 +1,8 @@
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class RestController{
 
