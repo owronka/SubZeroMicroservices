d62c90deb308edd64558e4e2f9a6a664c691b2da
Oliver Wronka <wronka@axxessio.com>
2020-05-15 15:51:18 +0200

    initial import

--- registration/src/main/java/com/axxessio/axx2cld/registration/ServiceController.java
+++ registration/src/main/java/com/axxessio/axx2cld/registration/ServiceController.java
@@ -1,20 +1,20 @@
 package com.axxessio.axx2cld.registration;
 
 
-	import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicLong;
 
-	import org.springframework.web.bind.annotation.GetMapping;
-	import org.springframework.web.bind.annotation.RequestParam;
-	import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-	@RestController
-	public class ServiceController {
+@RestController
+public class ServiceController {
 
-		private static final String template = "Hello, %s!";
-		private final AtomicLong counter = new AtomicLong();
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 
-		@GetMapping("/service")
-		public Service service(@RequestParam(value = "name", defaultValue = "World") String name) {
-			return new Service(counter.incrementAndGet(), String.format(template, name));
-		}
+	@GetMapping("/service")
+	public Service service(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Service(counter.incrementAndGet(), String.format(template, name));
+	}
 }
