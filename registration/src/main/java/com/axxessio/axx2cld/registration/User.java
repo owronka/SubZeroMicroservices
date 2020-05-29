c7bab08f11719fe9b4c785fb34d900715db3ccaa
Alihan Eray <alihan-eray@hotmail.de>
2020-05-29 16:26:10 +0200

    update

--- registration/src/main/java/com/axxessio/axx2cld/registration/User.java
+++ registration/src/main/java/com/axxessio/axx2cld/registration/User.java
@@ -1,8 +1,15 @@
 package com.axxessio.axx2cld.registration;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-	
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+	@Id
+	  @GeneratedValue(strategy=GenerationType.AUTO)
+
 	private String vorname;
 	private String name;
 	private String email;
--- registration/src/main/java/com/axxessio/axx2cld/registration/UserController.java
+++ registration/src/main/java/com/axxessio/axx2cld/registration/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ServiceController {
+public class UserController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
