f76133d181625db4ec0397bd13ac17e42e1fa447
Alihan Eray <alihan-eray@hotmail.de>
2020-06-12 16:07:10 +0200

    update

--- registration/src/main/java/com/axxessio/axx2cld/registration/PersonRepository.java
+++ registration/src/main/java/com/axxessio/axx2cld/registration/PersonRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.axxessio.axx2cld.registration.Person;
 
+
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
