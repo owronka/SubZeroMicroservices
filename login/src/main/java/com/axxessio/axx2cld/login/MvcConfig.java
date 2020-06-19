307f27acb2bca0f61c3748abebf6d98472c95ab1
jakin2s <jasemin.altundag@smail.inf.h-brs.de>
2020-06-19 14:12:46 +0200

    initial import

---
+++ login/src/main/java/com/axxessio/axx2cld/login/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.axxessio.axx2cld.login;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * This class extend WebMvcConfigurerAdapter and set the URL for a View.
+ * @author wronka
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
